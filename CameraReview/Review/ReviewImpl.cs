0461db5b153d5b16d55e91b80e8a4d344ab9e43d
German Bartoli <bartoligerman497@gmail.com>
2023-12-25 17:25:49 -0300

    Create ReviewImpl.cs

---
+++ CameraReview/Review/ReviewImpl.cs
@@ -0,0 +1,23 @@
+﻿using CameraReview.Product;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CameraReview.Review
+{
+    public class ReviewImpl : IReview
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public IProduct Product { get; set; }
+
+        public string GetContent()
+        {
+            return $"Title: {this.Title}\n" +
+                $"Content: {Content}\n" +
+                $"Author: {Author}" +
+                $"Product: {Product}" ;
+        }
+    }
+}
