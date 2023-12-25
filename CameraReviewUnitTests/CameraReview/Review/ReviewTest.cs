ce18ebf063be0ed22639efd45729a26909d94c38
German Bartoli <bartoligerman497@gmail.com>
2023-12-25 17:25:51 -0300

    Update ReviewTest.cs

--- CameraReviewUnitTests/CameraReview/Review/ReviewTest.cs
+++ CameraReviewUnitTests/CameraReview/Review/ReviewTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CameraReview.Product.Camera;
+using CameraReview.Review;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +11,31 @@ namespace CameraReviewUnitTests.CameraReview.Review
     /// Usar esta clase para escribir pruebas de Unitarias que corroboren la funcionalidad
     /// de los archivos en el namespace "Review"
     /// </summary>
-    class ReviewTest
+    [TestClass]
+    public class ReviewTest
     {
+
+        [TestMethod]
+        public void TestReviewContentMatchWithProduct()
+        {
+            // setup
+            Camera cameraProduct = new Camera();
+            cameraProduct.Name = "Canon 60D";
+            cameraProduct.Type = "FullFrame";
+            cameraProduct.CropFactor = 1;
+
+            ReviewImpl review = new ReviewImpl();
+            review.Title = "Titulo del review";
+            review.Author = "Un autor";
+            review.Product = cameraProduct;
+
+            // exec
+            var content = review.GetContent();
+
+            // assert
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(content), "Should return content but obtained null or whitespace.");
+            //Assert.IsTrue(content["products"].Contains(cameraProduct), "Should match content with the product.");
+
+        }
     }
 }
